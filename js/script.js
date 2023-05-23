43c84f0a694e503b2734e1c3398dae3adb52e981
quentin damois <quentin.damois@efrei.net>
2023-05-23 14:41:43 +0200

    Changes to be committed: 	modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -10,104 +10,18 @@ var minuscule = "abcdefghijklmnopqrstuvwxyz";
 var majuscule = "ABCDEFGHIJKLMONPQRSTUVWXYZ"
 var chiffre = "0123456789";
 var carspecial = "%!&*^()#$:";
-/*
-partie 2 : Récupérer le contenu du DOM : échauffement
-function generer() {
-    var monFormulaire = document.forms.addPWD;//forms['addPWD'];
-
-    console.log(monFormulaire.number.value);
-    var newLine = document.createElement("tr");
-    var col1 = document.createElement("td");
-    var col2 = document.createElement("td");
-    var col3 = document.createElement("td");
-    var col4 = document.createElement("td");
-    var col5 = document.createElement("td");
-    col1.textContent = "val1";
-    col2.textContent = "val2";
-    col3.textContent = "val3";
-    col4.textContent = "val4";
-    col5.textContent = "val5";
-    newLine.append(col1);
-    newLine.append(col2);
-    newLine.append(col3);
-    newLine.append(col4);
-    newLine.append(col5);
-    var monTableau = document.getElementById("montab");
-    monTableau.appendChild(newLine);
-
+function incrementerDuree() {
+    durees = document.getElementsByClassName("duree");
+    if (durees.lenght != 0) {
+        Array.prototype.forEach.call(durees, function(dureeElement) {
+            let valeur = parseInt(dureelement.textCotent);
+            dureeElement.textContent = valeur + 1;
+        })
+    }
 }
-*/
-/*
-partie 2 : Récupérer le contenu du DOM : La focntion de génération du mot de passe
-function generer() {
-    
-    var monFormulaire = document.forms.ajoutPWD;//forms['addPWD'];
-    //console.log(monFormulaire.nombre_caractere.value);
-    var password = "";
-    var listecar = "";
-    if (monFormulaire.elements["minuscule"].checked) {
-        listecar = listecar + minuscule;
-    }
-    if (monFormulaire.elements["majuscule"].checked) {
-        listecar = listecar + majuscule;
-    }
-    if (monFormulaire.elements["chiffre"].checked) {
-        listecar = listecar + chiffre;
-    }
-    if (monFormulaire.elements["symbole"].checked) {
-        listecar = listecar + carspecial;
-    }
-    //console.log(listecar.lenght);
-    for (var i  = 1; i <= monFormulaire.elements["number"].value; i++) {
-        var randomNumber = Math.floor(Math.random() * listecar.length);
-        password = password + listecar.substring(randomNumber, randomNumber + 1);
-    }
-    console.log(password);}
-    */
-/*
-Travail à faire : Ajout du mot de passe généré au tableau
-function generer() {
-    
-    var monFormulaire = document.forms.ajoutPWD;//forms['addPWD'];
-    //console.log(monFormulaire.nombre_caractere.value);
-    var password = "";
-    var listecar = "";
-    if (monFormulaire.elements["minuscule"].checked) {
-        listecar = listecar + minuscule;
-    }
-    if (monFormulaire.elements["majuscule"].checked) {
-        listecar = listecar + majuscule;
-    }
-    if (monFormulaire.elements["chiffre"].checked) {
-        listecar = listecar + chiffre;
-    }
-    if (monFormulaire.elements["symbole"].checked) {
-        listecar = listecar + carspecial;
-    }
-    //console.log(listecar.lenght);
-    for (var i  = 1; i <= monFormulaire.elements["number"].value; i++) {
-        var randomNumber = Math.floor(Math.random() * listecar.length);
-        password = password + listecar.substring(randomNumber, randomNumber + 1);
-    }
-    console.log(password);
 
-    var newLine = document.createElement("tr");
-    var col1 = document.createElement("td");
-    var col2 = document.createElement("td");
-    var col3 = document.createElement("td");
-    var col4 = document.createElement("td");
-    var col5 = document.createElement("td");
-    col1.textContent = monFormulaire.elements["number"].value;
-    col2.textContent = monFormulaire.elements["Date de validité"].value;
-    col3.textContent = monFormulaire.elements["monselect"].value;
-    col4.textContent = monFormulaire.elements["Site"].value;
-    col5.textContent = password;
-    newLine.append(col1, col2, col3, col4, col5);
-    var monTableau = document.getElementById("montab");
-    monTableau.appendChild(newLine);}
-*/
+setInterval(incrementerDuree());
 
-//Challenge :
 function generer() {
     var monFormulaire = document.forms.ajoutPWD;//forms['addPWD'];
     //console.log(monFormulaire.nombre_caractere.value);
@@ -140,12 +54,15 @@ function generer() {
     var col3 = document.createElement("td");
     var col4 = document.createElement("td");
     var col5 = document.createElement("td");
+    var duree = document.createElement("td");
+    duree.classeList = "duree";
+    duree.textContent = "0";
     col1.textContent = monFormulaire.elements["number"].value;
     col2.textContent = monFormulaire.elements["Date de validité"].value;
     col3.textContent = monFormulaire.elements["monselect"].value;
     col4.textContent = monFormulaire.elements["Site"].value;
     col5.textContent = password;
-    newLine.append(col1, col2, col3, col4, col5);
+    newLine.append(col1, col2, col3, col4, col5, duree);
     var monTableau = document.getElementById("montab");
     monTableau.appendChild(newLine);}
 
